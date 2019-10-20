827738459e56d545af76b69470d55bfacce97079
Andrew Countryman <apcountryman@gmail.com>
2019-10-19 22:57:22 -0700

    Add get Git repository path utility
    Supports #1.

--- git-utilities.cmake
+++ git-utilities.cmake
@@ -16,3 +16,42 @@
 # Description: CMake Git utilities.
 
 cmake_minimum_required( VERSION 3.13.4 )
+
+# Get the path to the nearest Git repository in the source tree.
+#
+# If a Git repository has not been found by the time the search reaches CMAKE_SOURCE_DIR,
+# a fatal error will be reported.
+#
+# SYNOPSIS
+#     get_git_repository_path( <git_repository_path> )
+#
+# OPTIONS
+#     <git_repository_path>
+#         The variable to store the path to the Git repository in.
+#
+# EXAMPLES
+#     get_git_repository_path( GIT_REPOSITORY_PATH )
+function( get_git_repository_path git_repository_path )
+    set( source_directory "${CMAKE_CURRENT_SOURCE_DIR}" )
+    set( repository_path "${source_directory}/.git" )
+    while( NOT EXISTS "${repository_path}" )
+        if( source_directory STREQUAL CMAKE_SOURCE_DIR )
+            message( FATAL_ERROR "source tree does not contain a git repository" )
+        endif( source_directory STREQUAL CMAKE_SOURCE_DIR )
+
+        get_filename_component( source_directory "${source_directory}" DIRECTORY )
+        set( repository_path "${source_directory}/.git" )
+    endwhile( NOT EXISTS "${repository_path}" )
+
+    if( IS_DIRECTORY "${repository_path}" )
+        set( ${git_repository_path} "${repository_path}" PARENT_SCOPE )
+    else( IS_DIRECTORY "${repository_path}" )
+        file( READ "${repository_path}" repository_path )
+        string( REPLACE "gitdir: " "" repository_path "${repository_path}" )
+        string( STRIP "${repository_path}" repository_path )
+        set( repository "${source_directory}/${repository_path}" )
+        get_filename_component( repository_path "${repository_path}" ABSOLUTE )
+
+        set( ${git_repository_path} "${repository_path}" PARENT_SCOPE )
+    endif( IS_DIRECTORY "${repository_path}" )
+endfunction( get_git_repository_path )
